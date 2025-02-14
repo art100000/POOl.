8f4c1e69f20743399a75dcb4b058ea6bc920acc3
art100000 <artfa9370@gmail.com>
2025-02-14 22:53:34 +0000

    teste

---
+++ teste.java/teste.java
@@ -0,0 +1,16 @@
+// public class teste {
+//     public static void main(String[] args) throws Exception{
+
+//     String name = "arthur";
+//     int age = 20;
+//     for ( int i=0; i<age; i++){
+//         System.out.println("parabens pra vc," + name);
+//         }
+
+//     }
+// }
+public double calcula Imc(doble altura, double peso){
+    return peso/(altura * altura);
+}
+doubleresultadoImc = calcula Imc(1.70, 80)
+System.out.println(resultado Imc);
